b52eac032feb2544b897ac02150755358b8c4719
VictorYarosh <victoryarosh131@gmail.com>
2024-09-16 15:56:25 +0300

    add some page

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+// import AuthForm from '@/components/AuthForm'
+
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      SingUp
+      {/*<AuthForm type="sign-up" />*/}
+    </section>
+  );
+};
+
+export default SignUp;
